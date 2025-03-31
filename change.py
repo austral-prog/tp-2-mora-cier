edad4c7fabe906c317a0fc4a72583a8d6c72d08f
mora-cier <mcier@mail.austral.edu.ar>
2025-03-31 14:54:56 -0300

    Mora-Cier_change.py

--- change.py
+++ change.py
@@ -1,3 +1,25 @@
 def change():
     expense = 23.75
     money = 100
+
+    print("Ingresar gasto:")
+    print(expense)
+    print("Dinero recibido")
+    print(money)
+
+    print(f"\n") 
+
+    print("Vuelto")
+
+    print(f"\n") 
+
+    print("Pesos:")
+    vuelto = money - expense
+    pesos = int(vuelto)
+    print(pesos)
+
+    print("Centavos:")
+    centavos = int((vuelto - pesos) * 100)
+    print(centavos)
+
+change()
